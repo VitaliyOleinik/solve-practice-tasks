72f513ac5289c27192ec733ed857ee1cf1d1be0b
Vitaliy Oleinik <oleynik624@gmail.com>
2022-11-28 21:12:43 +0600

    problem with reverse each word in string

---
+++ JavaSolvePracticeTasks/src/com/tasks/strings_numbers_math/InversionLettersAndWords.java
@@ -0,0 +1,37 @@
+package com.tasks.strings_numbers_math;
+
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class InversionLettersAndWords {
+    private static final String WHITESPACE = " ";
+
+    public static void main(String[] args) {
+        System.out.println(reverseWords("Привет! Как дела?"));
+        System.out.println(reverseWordsStreams("Нормально, а у тебя как дела?"));
+    }
+
+    private static String reverseWords(String string) {
+        String[] words = string.split(WHITESPACE);
+        StringBuilder reversedString = new StringBuilder();
+
+        for (String word : words) {
+            StringBuilder reverseWord = new StringBuilder();
+
+            for (int i = word.length() - 1; i >= 0; i--) {
+                reverseWord.append(word.charAt(i));
+            }
+            reversedString.append(reverseWord).append(WHITESPACE);
+        }
+
+        return reversedString.toString();
+    }
+
+    private static final Pattern PATTERN = Pattern.compile(" +");
+
+    private static String reverseWordsStreams(String string) {
+        return PATTERN.splitAsStream(string)
+                .map(w-> new StringBuilder(w).reverse())
+                .collect(Collectors.joining(" "));
+    }
+}
