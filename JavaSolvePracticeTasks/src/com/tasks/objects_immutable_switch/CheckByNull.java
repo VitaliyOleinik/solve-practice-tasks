46572003861de94d12b60a7da8673b3945b2ee77
Vitaliy Oleinik <oleynik624@gmail.com>
2022-12-12 00:08:59 +0600

    check by null

--- JavaSolvePracticeTasks/src/com/tasks/objects_immutable_switch/CheckByNull.java
+++ JavaSolvePracticeTasks/src/com/tasks/objects_immutable_switch/CheckByNull.java
@@ -6,7 +6,7 @@ public class CheckByNull {
     static List<Integer> numbers
             = Arrays.asList(1, 2, null, 4, null, 16, 7, null);
 
-    public static List<Integer> evenIntegers(List<Integer> integers) {
+    private static List<Integer> evenIntegers(List<Integer> integers) {
         if (Objects.isNull(integers)) {
             return Collections.EMPTY_LIST;
         }
@@ -19,13 +19,31 @@ public class CheckByNull {
         return evens;
     }
 
-    public static int sumIntegers(List<Integer> integers) {
+    private static int sumIntegers(List<Integer> integers) {
         if (integers == null) {
             throw new IllegalArgumentException("List cannot be null");
         }
         return integers.stream()
                 .filter(i -> i != null)
                 .mapToInt(Integer::valueOf).sum();
+    }
+
+    private static boolean integersContainsNulls(List<Integer> integers) {
+        if (integers == null) {
+            return false;
+        }
+
+        return integers.stream()
+                .anyMatch(i -> i == null);
+    }
+
+    public static int sumIntegersStreams(List<Integer> integers) {
+        if (integers == null) {
+            throw new IllegalArgumentException("List cannot be null");
+        }
+        return integers.stream()
+                .filter(Objects::nonNull)
+                .mapToInt(Integer::intValue).sum();
     }
 }
 
