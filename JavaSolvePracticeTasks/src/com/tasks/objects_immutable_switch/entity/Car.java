9a88fb51a1c2449c0583457cca9a5ec43da6462a
Vitaliy Oleinik <oleynik624@gmail.com>
2022-12-13 07:54:37 +0600

    check by null

---
+++ JavaSolvePracticeTasks/src/com/tasks/objects_immutable_switch/entity/Car.java
@@ -0,0 +1,52 @@
+package com.tasks.objects_immutable_switch.entity;
+
+import java.awt.*;
+import java.util.Objects;
+
+public class Car {
+    private final String name;
+    private final Color color;
+    private final String code;
+    private final String nameSupplier;
+
+    public Car(String name, Color color, String nameSupplier) {
+        if (name == null) {
+            throw new NullPointerException("Имя авто не может быть - null!");
+        }
+        if (color == null) {
+            throw new NullPointerException("Цвет авто не может быть - null!");
+        }
+        this.name = name;
+        this.color = color;
+        this.code = "1";
+        this.nameSupplier = Objects.requireNonNull(nameSupplier, () ->
+                "Имя автомобиля не может быть null . .. Возьмите имя из " + name);
+    }
+
+
+
+    public Car(String name, Color color, String code, String nameSupplier) {
+        this.name = Objects.requireNonNull(name,
+                "Имя авто не может быть - null!");
+        this.color = Objects.requireNonNull(color,
+                "Цвет авто не может быть - null!");
+        this.code = Objects.requireNonNull(code,
+                "code не может быть - null");
+        this.nameSupplier = Objects.requireNonNull(nameSupplier, () ->
+                "Имя автомобиля не может быть null . .. Возьмите имя из " + name);
+    }
+
+    public void assignDriver(String license, Point location) {
+        if (license == null) {
+            throw new NullPointerException("Лицензия не может быть - null");
+        }
+        if (location == null) {
+            throw new NullPointerException("Местоположение не может быть - null");
+        }
+    }
+
+    public void assignDriverRequireNonNull(String license, Point location) {
+        Objects.requireNonNull(license, "Лицензия не может быть null");
+        Objects.requireNonNull(location, "Местоположение не может быть null");
+    }
+}
