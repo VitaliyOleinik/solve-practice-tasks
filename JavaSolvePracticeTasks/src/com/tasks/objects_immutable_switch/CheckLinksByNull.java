a56e09e0f96f47aa521839f4a4c3046e15d30c31
Vitaliy Oleinik <oleynik624@gmail.com>
2022-12-15 23:07:35 +0600

    check by null

---
+++ JavaSolvePracticeTasks/src/com/tasks/objects_immutable_switch/CheckLinksByNull.java
@@ -0,0 +1,32 @@
+package com.tasks.objects_immutable_switch;
+
+import java.awt.*;
+import java.util.Objects;
+
+public class CheckLinksByNull {
+
+}
+
+class Car {
+    private final String name;
+    private final Color color;
+
+    public Car(String name, Color color) {
+        this.name = Objects.requireNonNullElse(name, "No name");
+        this.color = Objects.requireNonNullElse(color, new Color(0, 0, 0));
+    }
+
+    public Car(String name, Color color, String a) {
+        if (name == null) {
+            this.name = "Безымянный";
+        } else {
+            this.name = name;
+        }
+        if (color == null) {
+            this.color = new Color(0, 0, 0);
+        } else {
+            this.color = color;
+        }
+    }
+}
+
