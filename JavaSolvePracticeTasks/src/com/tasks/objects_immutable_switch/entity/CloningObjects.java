2e0f737c53b84d36c558c97699bb518f37be6f9a
Vitaliy Oleinik <oleynik624@gmail.com>
2022-12-29 07:50:44 +0600

    clone

---
+++ JavaSolvePracticeTasks/src/com/tasks/objects_immutable_switch/entity/CloningObjects.java
@@ -0,0 +1,13 @@
+package com.tasks.objects_immutable_switch.entity;
+
+public class CloningObjects {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Point point = new Point(1,2);
+        Point clone = point.clonePoint();
+        Point cloneWithCloneable = (Point) point.clone();
+        // deep cloning using class Cloner
+        // Cloner.cloner.deepClone(point)
+        Point point1 = new Point(3,4);
+
+    }
+}
---
+++ JavaSolvePracticeTasks/src/com/tasks/objects_immutable_switch/entity/Point.java
@@ -0,0 +1,49 @@
+package com.tasks.objects_immutable_switch.entity;
+
+public class Point implements Cloneable {
+    private double x;
+    private double y;
+
+    public Point() {
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public Point clonePoint() {
+        Point point = new Point();
+        point.setX(this.x);
+        point.setY(this.y);
+        return point;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+    //example of cloning with GSON
+//    private static <T> T cloneThroughJson(T t) {
+//        Gson gson = new Gson();
+//        String json = gson.toJson(t);
+//
+//        return (T) gson.fromJson(json, t.getClass());
+//    }
+}
