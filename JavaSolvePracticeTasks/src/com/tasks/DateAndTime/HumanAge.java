9d6878633bce4e934beb8cd4f0b35fc262659669
Vitaliy Oleinik <oleynik624@gmail.com>
2023-01-03 11:33:37 +0600

    max with Generics

--- JavaSolvePracticeTasks/src/com/tasks/DateAndTime/HumanAge.java
+++ JavaSolvePracticeTasks/src/com/tasks/DateAndTime/HumanAge.java
@@ -3,6 +3,7 @@ package com.tasks.DateAndTime;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
 
 public class HumanAge {
     public static void main(String[] args) {
@@ -10,5 +11,16 @@ public class HumanAge {
         LocalDate endLocalDate = LocalDate.now();
         long years = ChronoUnit.YEARS.between(startLocalDate, endLocalDate);
         Period periodBetween = Period.between(startLocalDate, endLocalDate);
+    }
+
+    public static <T> T max(T[] arr, Comparator<? super T> c) {
+        T max = arr[0];
+
+        for (T elem: arr) {
+            if (c.compare(elem, max) > 0) {
+                max = elem;
+            }
+        }
+        return max;
     }
 }
