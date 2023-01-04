edab8e00be0fa00bd64ddf250071484519b0368e
Vitaliy Oleinik <oleynik624@gmail.com>
2023-01-04 23:57:07 +0600

    Compute if absent

---
+++ JavaSolvePracticeTasks/src/com/tasks/array_collections_data_structure/ComputeIfAbsent.java
@@ -0,0 +1,24 @@
+package com.tasks.array_collections_data_structure;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class ComputeIfAbsent {
+    public static void main(String[] args) throws UnknownHostException {
+        Map<String, String> map = new HashMap<>();
+        map.put("postgresql", "jdbc:postgresql://127.0.0.l/customers_db");
+        map.put("mysql", "jdbc:mysql://192.168.0.50/customers_db");
+
+        String address = InetAddress.getLocalHost().getHostAddress();
+        Function<String, String> jdbcUrl
+                = k -> k +"://" + address + "/customers_db";
+        System.out.println(address);
+        System.out.println(jdbcUrl);
+        // mongodb://192.168.100.10/customers_db
+        String mongodbJdbcUrl = map.computeIfAbsent("mongodb", jdbcUrl);
+        System.out.println(mongodbJdbcUrl);
+    }
+}
