38f55a5d04cde2b735221bf4a20137ed17db7e13
Vitaliy Oleinik <oleynik624@gmail.com>
2023-01-08 21:01:33 +0600

    remove from map

--- JavaSolvePracticeTasks/src/com/tasks/array_collections_data_structure/DeleteElementFromViewOfMap.java
+++ JavaSolvePracticeTasks/src/com/tasks/array_collections_data_structure/DeleteElementFromViewOfMap.java
@@ -9,7 +9,14 @@ public class DeleteElementFromViewOfMap {
         map.put(1, "postgresql");
         map.put(2, "mysql");
         map.put(3, "derby");
-        String r1 = map.remove(1);
-        String r2 = map.remove(4);
+        String r1 = map.remove(1); // postgresql
+        String r2 = map.remove(4); // null
+        // map of 2 and 3 keys
+        // true
+        boolean ex1 = map.remove(2, "mysql");
+        // false (key is exist and value not equal)
+        boolean ex2 = map.remove(3, "mysql");
+        // key: 3 value: derby
+        map.forEach((k, v) -> System.out.println("key: " + k + " value: " + v));
     }
 }
