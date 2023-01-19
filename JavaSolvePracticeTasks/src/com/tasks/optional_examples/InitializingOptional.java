d2f857d25cee9f67564c20dff6d1ac3e7585590d
Vitaliy Oleinik <oleynik624@gmail.com>
2023-01-19 20:47:02 +0600

    optional init

---
+++ JavaSolvePracticeTasks/src/com/tasks/optional_examples/InitializingOptional.java
@@ -0,0 +1,10 @@
+package com.tasks.optional_examples;
+
+import java.util.Optional;
+
+public class InitializingOptional {
+    public static void main(String[] args) {
+        Optional<String> book = Optional.empty();
+
+    }
+}
