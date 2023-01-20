ae6da0da09832f12ea9d1ef59e0445451fe43f83
Vitaliy Oleinik <oleynik624@gmail.com>
2023-01-20 21:45:02 +0600

    optional get, ifPresent

--- JavaSolvePracticeTasks/src/com/tasks/optional_examples/InitializingOptional.java
+++ JavaSolvePracticeTasks/src/com/tasks/optional_examples/InitializingOptional.java
@@ -1,10 +1,21 @@
 package com.tasks.optional_examples;
 
+import java.awt.print.Book;
 import java.util.Optional;
 
 public class InitializingOptional {
     public static void main(String[] args) {
         Optional<String> book = Optional.empty();
+        // Если "book" является пустым, то следующий ниже код
+        // выбросит исключение java.util.NoSuchElementException
+        String theBook = book.get();
 
+        // Предпочтительно
+        if (book.isPresent()) {
+            String theBook1 = book.get();
+            // сделать что-то c "theBook"
+        } else {
+            // сделать что-то, что не вызывает book.getO
+        }
     }
 }
