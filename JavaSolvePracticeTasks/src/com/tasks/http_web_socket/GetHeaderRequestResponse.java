6f3cd0d2e38e9dc1b35883a730f9c3cbc0dbb402
Vitaliy Oleinik <oleynik624@gmail.com>
2023-01-30 18:44:51 +0600

    settingsOfMethodsHTTP

--- JavaSolvePracticeTasks/src/com/tasks/http_web_socket/GetHeaderRequestResponse.java
+++ JavaSolvePracticeTasks/src/com/tasks/http_web_socket/GetHeaderRequestResponse.java
@@ -1,6 +1,8 @@
 package com.tasks.http_web_socket;
 
+import java.net.URI;
 import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.Optional;
@@ -13,5 +15,31 @@ public class GetHeaderRequestResponse {
                 = response.headers().allValues("Cache-Control");
         Optional<String> firstValueOfCacheControl
                 = response.headers().firstValue("Cache-Control");
+    }
+
+    private void settingsOfMethodsHTTP() {
+        HttpRequest requestGet = HttpRequest.newBuilder()
+                .GET() // can be omitted since it is default
+                .uri(URI.create("https://reqres.in/api/users/2"))
+                .build();
+
+        HttpRequest requestPost = HttpRequest.newBuilder()
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(
+                        "{\"name\": \"morpheus\",\"job\": \"leader\"}"))
+                .uri(URI.create("https://reqres.in/api/users"))
+                .build();
+
+        HttpRequest requestPut = HttpRequest.newBuilder()
+                .header("Content-Type", "application/json")
+                .PUT(HttpRequest.BodyPublishers.ofString(
+                        "{\"name\": \"morpheus\",\"job\": \"zion resident\"}"))
+                .uri(URI.create("https://reqres.in/api/users/2"))
+                .build();
+
+        HttpRequest requestDelete = HttpRequest.newBuilder()
+                .DELETE()
+                .uri(URI.create("https://reqres.in/api/users/2"))
+                .build();
     }
 }
